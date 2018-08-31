b1358c05f91865cf3837a455570a2990846ab822
Charlie Fish <fishcharlie@me.com>
2018-08-31 16:44:49 -0600

    First commit

--- TableViewTest1/ViewController.swift
+++ TableViewTest1/ViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 150
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = "Test \(indexPath)"
+        return cell
+    }
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    private lazy var refreshControl = UIRefreshControl()
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        refreshControl.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+        tableView.refreshControl = refreshControl
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @objc func handleRefresh() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            self.refreshControl.endRefreshing()
+        }
+    }
 
 }
-
